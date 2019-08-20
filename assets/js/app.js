518b141680e4b5eaaab33a07bf4c0a5e351c5c0b
Ronny Kruebel <ronnykruebel@outlook.com>
2019-08-20 14:01:05 +0200

    add special css effects

--- assets/js/app.js
+++ assets/js/app.js
@@ -6,7 +6,9 @@
  */
 
 // any CSS you require will output into a single css file (app.css in this case)
-//equire('../css/app.css');
+// require('../css/app.css');
+require('@fortawesome/fontawesome-free/css/all.min.css');
+require('@fortawesome/fontawesome-free/js/all.js');
 // Need jQuery? Install it with "yarn add jquery", then uncomment to require it.
 // const $ = require('jquery');
 
