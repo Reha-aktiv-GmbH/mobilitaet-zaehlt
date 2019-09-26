51d83e6ee622adfd85658bc8249ea680ab63d50e
Ronny Kruebel <ronnykruebel@outlook.com>
2019-09-26 21:00:47 +0200

    add policy

--- src/Controller/Frontend/StartController.php
+++ src/Controller/Frontend/StartController.php
@@ -5,15 +5,38 @@ namespace App\Controller\Frontend;
 
 
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
 
 class StartController extends AbstractController
 {
     /**
      * @Route("/",name="home",methods={"GET"})
+     *
+     * @return Response
      */
     public function index()
     {
         return $this->render('frontend/start/index.html.twig');
+    }
+
+    /**
+     * @Route("/datenschutz",name="policy",methods={"GET"})
+     *
+     * @return Response
+     */
+    public function policy()
+    {
+        return $this->render('frontend/start/policy.html.twig');
+    }
+
+    /**
+     * @Route("/impressum",name="imprint",methods={"GET"})
+     *
+     * @return Response
+     */
+    public function imprint()
+    {
+        return $this->render('frontend/start/imprint.html.twig');
     }
 }
